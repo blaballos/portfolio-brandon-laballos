c79f044347bde37c2c6852f060281bfa9bf80c1f
blaballos <laballosbrandon@gmail.com>
2025-02-25 16:28:11 -0300

    Add: new font family for the page ➕

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				HeyComic: ["'HeyComic'", "Onest Variable"], 
+				SavoryCurry: ["'SavoryCurry'", "Onest Variable"],
+			},
+		},
 	},
 	plugins: [],
 }
